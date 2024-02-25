ef38419320ba940ce29a9e4de44d6d290f6aa64b
Dima-Chuchman <dmytrochuchman@knu.ua>
2024-02-25 18:49:40 +0200

    push

--- utils.py
+++ utils.py
@@ -11,3 +11,20 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+<<<<<<< Updated upstream
+=======
+def is_power_of_five(n):
+    if n == 1:
+        return True
+    if n < 1 or n % 5 != 0:
+        return False
+    return is_power_of_five(n/5)
+
+print(is_power_of_five(125))
+def is_power_of_two(n):
+    if n == 1:
+        return True
+    if n < 1 or n % 2 != 0:
+        return False
+    return is_power_of_two(n/2)
+>>>>>>> Stashed changes
